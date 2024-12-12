eb1e0fb2548c3e075bc7c2be091c6c2c7b301d93
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-12-13 02:26:56 +0530

    add 'choose date and time' screen

--- app/report.jsx
+++ app/report.jsx
@@ -2,9 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, ScrollView, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import DatePicker from "react-native-modern-datepicker";
+
 
 const ChooseTypeScreen = () => {
-  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [step, setStep] = useState(0);
+  const [formData, setFormData] = useState({
+    types: [],
+    date: '',
+  });
   const types = [
     "Verbal Abuse",
     "Sexual Harassment",
@@ -15,63 +21,94 @@ const ChooseTypeScreen = () => {
     "Abuse of Authority by Staff or Faculty"
   ];
 
-  const toggleSelection = (type) => {
-    if (selectedTypes.includes(type)) {
-      setSelectedTypes((prev) => prev.filter((t) => t !== type));
+  const toggleTypes = (type) => {
+    if (formData.types.includes(type)) {
+      setFormData({...formData, types: formData.types.filter((t) => t !== type)});
     } else {
-      setSelectedTypes((prev) => [...prev, type]);
+      setFormData({...formData, types: [...formData.types, type]});
     }
   };
+  switch (step) {
+    case 0:
+      return (
+        <SafeAreaView className="flex-1 bg-white p-7 min-h-full">
+          <View>
+            <Text className="text-left text-3xl mt-10 font-pregular">Choose a type</Text>
+          </View>
 
-  return (
-    <SafeAreaView className="flex-1 bg-white p-7 min-h-full">
-      <View className="">
-        <Text className="text-left text-3xl mt-10 font-pregular">Choose a type</Text>
-      </View>
-
-      {/* Selectable Items */}
-      <ScrollView
-        contentContainerStyle={{
-          paddingVertical: 20,
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-        }}
-      >
-        {types.map((type, index) => (
-          <Pressable
-            key={index}
-            className={`px-4 mb-3 rounded-lg  border w-full ${
-              selectedTypes.includes(type)
-                ? "bg-mylavender border-transparent"
-                : "bg-white border-gray-300"
-            }`}
-            style={{ paddingVertical: 15, backgroundColor: "#" }}
-            onPress={() => toggleSelection(type)}
+          <ScrollView
+            contentContainerStyle={{
+              paddingVertical: 20,
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+            }}
           >
-            <Text
-              className={`font-plight ${
-                selectedTypes.includes(type) ? "text-white" : "text-grey-600"
-              }`}
-            >
-              {type}
-            </Text>
-          </Pressable>
-        ))}
-      </ScrollView>
+            {types.map((type, index) => (
+              <Pressable
+                key={index}
+                className={`px-4 mb-3 rounded-lg  border w-full transition-all duration-700 ${
+                  formData.types.includes(type)
+                    ? "bg-mypink border-transparent"
+                    : "bg-white border-gray-300"
+                }`}
+                style={{ paddingVertical: 15 }}
+                onPress={() => toggleTypes(type)}
+              >
+                <Text
+                  className={`font-plight ${
+                    formData.types.includes(type) ? "text-white" : "text-grey-600"
+                  }`}
+                >
+                  {type}
+                </Text>
+              </Pressable>
+            ))}
+          </ScrollView>
 
-      {/* Next Button */}
-      <View className="">
-        <Pressable
-          className="py-4 bg-mypink rounded-xl w-full"
-          onPress={() => alert(`Selected Types: ${selectedTypes.join(", ")}`)}
-        > 
-          <Text className="text-center text-lg font-bold text-white">Next</Text>
-        </Pressable>
-      </View>
-    <StatusBar style="auto light" />
-    </SafeAreaView>
-  );
+          {/* Next Button */}
+          <View className="">
+            <Pressable
+              className={`py-4 rounded-xl w-full bg-mylavender`}
+              onPress={() => setStep(1)}
+              disabled={formData.types.length === 0}
+              style={{opacity : (formData.types.length === 0 ? 0.5 : 1)}}
+            > 
+              <Text className="text-center text-lg font-bold text-white">Next</Text>
+            </Pressable>
+          </View>
+        <StatusBar style="auto light" />
+        </SafeAreaView>
+      );
+    case 1:
+      return (
+        <SafeAreaView className="flex-1 bg-white p-7 min-h-full">
+          <View>
+            <Text className="text-left text-3xl mt-10 font-pregular">Enter Date and Time</Text>
+          </View>
+
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+          >
+            <DatePicker mode='time' onTimeChange={(date) => setFormData({...formData, date})} style={{ marginBottom: -120 }} />
+            <DatePicker mode='calendar' onTimeChange={(date) => setFormData({...formData, date})} selected="23" />
+          </ScrollView>
+
+          {/* Next Button */}
+          <View className="">
+            <Pressable
+              className={`py-4 rounded-xl w-full bg-mylavender`}
+              onPress={() => setStep(1)}
+              disabled={formData.types.length === 0}
+              style={{opacity : (formData.types.length === 0 ? 0.5 : 1)}}
+            > 
+              <Text className="text-center text-lg font-bold text-white">Next</Text>
+            </Pressable>
+          </View>
+        <StatusBar style="auto light" />
+        </SafeAreaView>
+      );
+  }
 };
 
 export default ChooseTypeScreen;
