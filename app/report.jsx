6343c7f212d73693d355a67eb102d886eb1aab44
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-12-13 14:17:30 +0530

    change location datetime screen and add upload images screen

--- app/report.jsx
+++ app/report.jsx
@@ -1,16 +1,51 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, ScrollView, Pressable } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import DatePicker from "react-native-modern-datepicker";
-
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Pressable,
+  TextInput,
+  Image,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import FormScreen from "../components/FormScreen";
+import * as ImagePicker from "expo-image-picker";
 
 const ChooseTypeScreen = () => {
   const [step, setStep] = useState(0);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+
   const [formData, setFormData] = useState({
     types: [],
-    date: '',
+    date: new Date(),
+    time: new Date(),
+    location: "",
   });
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsMultipleSelection: true,
+
+    });
+    if (!result.canceled) {
+      setFormData((prev) => ({ ...prev, uploadedImage: result.assets[0].uri }));
+    }
+  };
+
+
+  const onTimeChange = (event, selectedTime) => {
+    const currentTime = selectedTime || time;
+    setShowTimePicker(false);
+    setFormData({ ...formData, time: currentTime });
+  };
+
+  const onDateChange = (event, selectedDate) => {
+    setShowDatePicker(false);
+    setFormData({ ...formData, date: selectedDate });
+  };
+
   const types = [
     "Verbal Abuse",
     "Sexual Harassment",
@@ -18,36 +53,27 @@ const ChooseTypeScreen = () => {
     "Stalking",
     "Cyber harassment",
     "Discrimination",
-    "Abuse of Authority by Staff or Faculty"
+    "Abuse of Authority by Staff or Faculty",
   ];
 
   const toggleTypes = (type) => {
     if (formData.types.includes(type)) {
-      setFormData({...formData, types: formData.types.filter((t) => t !== type)});
+      setFormData({
+        ...formData,
+        types: formData.types.filter((t) => t !== type),
+      });
     } else {
-      setFormData({...formData, types: [...formData.types, type]});
+      setFormData({ ...formData, types: [...formData.types, type] });
     }
   };
   switch (step) {
     case 0:
       return (
-        <SafeAreaView className="flex-1 bg-white p-7 min-h-full">
-          <View>
-            <Text className="text-left text-3xl mt-10 font-pregular">Choose a type</Text>
-          </View>
-
-          <ScrollView
-            contentContainerStyle={{
-              paddingVertical: 20,
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100%",
-            }}
-          >
+        <FormScreen heading="Choose Type" disabledContidion={formData.types.length === 0} setStep={setStep}>
             {types.map((type, index) => (
               <Pressable
                 key={index}
-                className={`px-4 mb-3 rounded-lg  border w-full transition-all duration-700 ${
+                className={`px-4 mb-3 rounded-lg  border w-full transition-all duration-500 ${
                   formData.types.includes(type)
                     ? "bg-mypink border-transparent"
                     : "bg-white border-gray-300"
@@ -57,56 +83,97 @@ const ChooseTypeScreen = () => {
               >
                 <Text
                   className={`font-plight ${
-                    formData.types.includes(type) ? "text-white" : "text-grey-600"
+                    formData.types.includes(type)
+                      ? "text-white"
+                      : "text-grey-600"
                   }`}
                 >
                   {type}
                 </Text>
               </Pressable>
             ))}
-          </ScrollView>
-
-          {/* Next Button */}
-          <View className="">
-            <Pressable
-              className={`py-4 rounded-xl w-full bg-mylavender`}
-              onPress={() => setStep(1)}
-              disabled={formData.types.length === 0}
-              style={{opacity : (formData.types.length === 0 ? 0.5 : 1)}}
-            > 
-              <Text className="text-center text-lg font-bold text-white">Next</Text>
-            </Pressable>
-          </View>
-        <StatusBar style="auto light" />
-        </SafeAreaView>
+        </FormScreen>
       );
     case 1:
       return (
-        <SafeAreaView className="flex-1 bg-white p-7 min-h-full">
+        <FormScreen heading="Location and Time" disabledContidion={formData.location === ""} setStep={setStep}>
+            <View className="mb-4 w-full">
+              <Text className="font-pregular text-gray-800 mb-2">
+                Location
+              </Text>
+              <TextInput
+                className="px-6 py-4 mb-3 rounded-lg text-lg bg-white border border-gray-300 text-gray-800 "
+                placeholder="Enter location"
+                value={formData.location}
+                onChangeText={(text) =>
+                  setFormData({ ...formData, location: text })
+                }
+              />
+            </View>
+            <View className="mb-4 w-full">
+              <Text className="font-pregular text-gray-800 mb-2">
+                Time
+              </Text>
+              <TouchableOpacity
+                className="px-6 py-5 mb-3 text-lg font-plight bg-white border border-gray-300 rounded-lg"
+                onPress={() => setShowTimePicker(true)}
+              >
+                <Text className="text-gray-800">
+                  {formData.time.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </Text>
+              </TouchableOpacity>
+              {showTimePicker && (
+                <DateTimePicker
+                  value={formData.time}
+                  mode="time"
+                  is24Hour={false}
+                  display="default"
+                  onChange={onTimeChange}
+                />
+              )}
+            </View>
+            <View className="mb-4 w-full">
+              <Text className="font-pregular text-gray-800 mb-2">
+                Date
+              </Text>
+              <TouchableOpacity
+                className="py-5 px-5 bg-white text-lg font-plight border border-gray-300 rounded-lg w-full"
+                onPress={() => setShowDatePicker(true)}
+              >
+                <Text className="text-gray-800">
+                  {formData.date.toLocaleDateString([], {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </Text>
+              </TouchableOpacity>
+              {showDatePicker && (
+                <DateTimePicker
+                  value={formData.date}
+                  mode="date"
+                  display="default"
+                  onChange={onDateChange}
+                />
+              )}
+            </View>
+        </FormScreen>
+      );
+    case 2:
+      return (
+        <FormScreen heading="Upload Evidence" disabledContidion={false} setStep={setStep}>
           <View>
-            <Text className="text-left text-3xl mt-10 font-pregular">Enter Date and Time</Text>
+          <TouchableOpacity
+              onPress={pickImage}
+              className="bg-gray-200 py-3 px-4 rounded-lg mb-4"
+            >
+              <Text className="text-blue-500">Upload Image</Text>
+            </TouchableOpacity>
           </View>
-
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-          >
-            <DatePicker mode='time' onTimeChange={(date) => setFormData({...formData, date})} style={{ marginBottom: -120 }} />
-            <DatePicker mode='calendar' onTimeChange={(date) => setFormData({...formData, date})} selected="23" />
-          </ScrollView>
-
-          {/* Next Button */}
-          <View className="">
-            <Pressable
-              className={`py-4 rounded-xl w-full bg-mylavender`}
-              onPress={() => setStep(1)}
-              disabled={formData.types.length === 0}
-              style={{opacity : (formData.types.length === 0 ? 0.5 : 1)}}
-            > 
-              <Text className="text-center text-lg font-bold text-white">Next</Text>
-            </Pressable>
-          </View>
-        <StatusBar style="auto light" />
-        </SafeAreaView>
+        </FormScreen>
       );
   }
 };
